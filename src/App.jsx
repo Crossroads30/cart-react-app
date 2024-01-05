59bd878cd419ff6cef8921a25f466321ee4d39ab
crossroads30 <crossroads30@mail.ru>
2024-01-05 18:07:07 +0300

    feat: add fetching data & error showing

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,27 @@ import CartContainer from './CartContainer'
 // items
 
 function App() {
-  // if (loading) {
-  //   return (
-  //     <div className='loading'>
-  //       <h1>Loading...</h1>
-  //     </div>
-  //   )
-  // }
-  return (
-    <main>
-      <Navbar />
-      <CartContainer />
-    </main>
-  )
+	const { isLoading, isError } = useGlobalContext()
+	if (isLoading) {
+	  return (
+	    <div className='loading'>
+	      <h1>Loading...</h1>
+	    </div>
+	  )
+	}
+	if (isError) {
+		return (
+			<div className='loading'>
+				<h1>Something went wrong...</h1>
+			</div>
+		)
+	}
+	return (
+		<main>
+			<Navbar />
+			<CartContainer />
+		</main>
+	)
 }
 
 export default App
--- src/context.jsx
+++ src/context.jsx
@@ -1,27 +1,54 @@
 import React, { useState, useContext, useReducer, useEffect } from 'react'
 import cartItems from './data'
-import reducer from './reducer'
+import { reducer } from './reducer'
+
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-useReducer-cart-project'
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+	const [cart, setCart] = useState(cartItems)
+	const [isLoading, setIsLoading] = useState(true)
+	const [isError, setIsError] = useState(false)
 
-  return (
-    <AppContext.Provider
-      value={{
-        cart,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  )
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch(url)
+				if (!response.ok) {
+					// case for 'fetch' to handle '400th' & '500th' errors!
+					setIsError(true)
+					setIsLoading(false)
+					return
+				}
+				const data = await response.json()
+				console.log(data)
+				setCart(data)
+			} catch (error) {
+				console.log(error)
+			}
+      setIsLoading(false)
+		}
+		fetchData()
+	}, [])
+
+	return (
+		<AppContext.Provider
+			value={{
+				cart,
+				isLoading,
+				isError,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	)
 }
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
+	return useContext(AppContext)
 }
 
 export { AppContext, AppProvider }
